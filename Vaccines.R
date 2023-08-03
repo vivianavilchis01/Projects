e4c7354fcd8b03f4377d0d3b852da0e064cfa72f
Viviana Vilchis <80860348+vivianavilchis01@users.noreply.github.com>
2023-08-02 20:44:10 -0600

    Update Vaccines.R

--- Vaccines.R
+++ Vaccines.R
@@ -22,7 +22,7 @@ promedio <- us_contagious_diseases %>%
   filter(disease == la_enfermedad) %>% group_by(year) %>%
   summarize(us_rate = sum(count, na.rm = TRUE)/sum(population, na.rm = TRUE)*10000)
 
-# hacer un gráfico de líneas de la tasa de sarampión por año por estado
+#hacer un gráfico de líneas de la tasa de sarampión por año por estado
 dat %>%
   filter(!is.na(rate)) %>%
   ggplot() +
@@ -31,9 +31,9 @@ dat %>%
   geom_line(mapping = aes(year, us_rate), data = promedio, size = 1, col = "hotpink") +
   scale_y_continuous(trans = "sqrt", breaks = c(5, 25, 125, 300)) +
   ggtitle("Casos por 10,000 por estado") +
+  theme(plot.title = element_text(color = "hotpink"))+
   xlab("") +
   ylab("") +
   geom_text(data = data.frame(x = 1955, y = 50),
             mapping = aes(x, y, label = "Promedio EE.UU."), color = "hotpink") +
-  geom_vline(xintercept = 1963, col = "hotpink")
-
+  geom_vline(xintercept = 1963, col = "red")
